f3bdb612b6ad3a1c4807a972341c36572e67207c
Charmelle Gonzales <charmelle.gonzales@gmail.com>
2022-09-19 17:52:05 -0500

    Add GameHelpers module

--- mastermind.rb
+++ mastermind.rb
@@ -1,22 +1,5 @@
-class String
-  def green; "\e[32m#{self}\e[0m" end
-  def red;   "\e[31m#{self}\e[0m" end
-  def blue; "\e[34m#{self}\e[0m" end
-  def magenta; "\e[35m#{self}\e[0m" end
-  def bold; "\e[1m#{self}\e[22m" end
-end
-
-class Game
-  def initialize  
-    @comp = Computer.new
-    @correct_num_position = 0
-    @correct_num = 0
-    @rounds = 1
-    instructions
-    user_choice
-  end
-
-  def instructions
+module GameHelpers
+  def intro
     puts 'Welcome to Mastermind! Ready for a code-breaking challenge?' \
     "\nTest the computer's code-breaking skills or your own." \
     "\n\nYou have 2 options: 1) Be a #{'CODE-MAKER'.red} or 2) Be a " \
@@ -29,66 +12,9 @@ class Game
    "\t○ = Correct number but wrong position\n"
   end
 
-  def user_choice ## move to main && break up the method
-    puts "\nEnter your choice: 1) Be a #{'CODE-MAKER'.red} 2) Be a " \
-    "#{'CODE-BREAKER'.blue}  "
-    while true
-      choice = gets.chomp.to_i
-      break if choice.between?(1, 2)
-
-      puts 'Invalid input!'
-    end
-    @human = Human.new
-    sleep 1 # move outside method into main
-    system('clear') # move outside method into main
-    play(choice) # invoke on game object & remove from this method
-  end
-
-  def play(choice)
-    case choice
-    when 1
-      codemaker_play
-    when 2
-      combo = @comp.generate_combo
-      codebreaker_loop(combo)
-    end
-  end
-
-  def valid_input?(input)
-    input.all? { |x| x.between?(1, 6) } && input.length == 4
-  end
-
-  protected
-
-  def codebreaker_loop(combo) # find a way to make this shorter
-    p combo
-    while @rounds <= 12
-      puts "\n\n#{'ROUND:'.magenta.bold} #{@rounds}"
-      user_combo = @human.prompt_user_combo
-      if match?(combo, user_combo)
-        puts 'You cracked the code!'; return
-      end
-      if valid_input?(user_combo)
-        codebreaker_play(user_combo, combo)
-      else
-        puts "\nInvalid input! Enter a number between 1-6"
-      end
-    end
-    lose_message
-  end
-
-  def codebreaker_play(user_combo, combo)
-    check_combinations(user_combo, combo)
-    display_feedback(@correct_num_position, @correct_num)
-    @rounds += 1
-    reset_counter
-  end
-
-  def lose_message
-    puts "\nYou lost! Play again? (Y/N)"
-    input = gets.chomp.upcase
-    initialize if input == 'Y'
-    exit
+  def clear_console
+    sleep 1
+    system('clear')
   end
 
   def display_feedback(num_pos, num)
@@ -99,32 +25,131 @@ class Game
       print '○ '
     end
   end
+end
 
-  def reset_counter
-    @correct_num_position = 0
-    @correct_num = 0
+class String
+  def green
+    "\e[32m#{self}\e[0m"
   end
 
-  def check_combinations(user_combo, combo)
-    user_combo.each_index do |i|
-      if user_combo[i].to_i == combo[i]
-        @correct_num_position += 1
-      elsif combo.include?(user_combo[i])
-        @correct_num += 1
-      end
+  def red
+    "\e[31m#{self}\e[0m"
+  end
+
+  def blue
+    "\e[34m#{self}\e[0m"
+  end
+
+  def magenta
+    "\e[35m#{self}\e[0m"
+  end
+
+  def bold
+    "\e[1m#{self}\e[22m"
+  end
+end
+
+class Game
+  include GameHelpers
+  def initialize
+    @comp = Computer.new
+    @human = Human.new
+    @correct_num_position = 0
+    @correct_num = 0
+    @rounds = 1
+    intro
+  end
+
+  def user_choice
+    puts "\nEnter your choice: 1) Be a #{'CODE-MAKER'.red} 2) Be a " \
+    "#{'CODE-BREAKER'.blue}  "
+    while true
+      choice = gets.chomp.to_i
+      break if choice.between?(1, 2)
+
+      puts 'Invalid input!'
+    end
+    choice
+  end
+
+  def play
+    choice = user_choice
+    @human.player_name
+    clear_console
+    case choice
+    when 1
+      codemaker_play
+    when 2
+      @combo = @comp.generate_combo
+      codebreaker_loop(@combo)
     end
   end
 
-  def match?(user_combo, combo)
-    true if user_combo == combo
+  def valid_input?(input)
+    input.all? { |x| x.between?(1, 6) } && input.length == 4
   end
+
+  protected
+
+  def codebreaker_loop(combo) # find a way to make this shorter
+    while @rounds <= 12
+      puts "\n\n#{'ROUND:'.magenta.bold} #{@rounds}"
+      user_combo = @human.prompt_user_combo
+      if match?(@combo, user_combo)
+        puts 'You cracked the code!'; return
+      end
+      if valid_input?(user_combo)
+        codebreaker_play(user_combo, @combo)
+      else
+        puts "\nInvalid input! Enter a number between 1-6"
+      end
+    end
+    puts "You lost! The secret code was #{@combo.join}"
+    play_again?
+  end
+
+  def codebreaker_play(user_combo, combo)
+    check_combinations(user_combo, combo)
+    display_feedback(@correct_num_position, @correct_num)
+    @rounds += 1
+    reset_counter
+  end
+end
+
+def reset_counter
+  @correct_num_position = 0
+  @correct_num = 0
+end
+
+def check_combinations(user_combo, combo)
+  user_combo.each_index do |i|
+    if user_combo[i].to_i == combo[i]
+      @correct_num_position += 1
+    elsif combo.include?(user_combo[i])
+      @correct_num += 1
+    end
+  end
+end
+
+def match?(user_combo, combo)
+  true if user_combo == combo
+end
+
+def play_again?
+  puts 'Play again? (Y/N)'
+  input = gets.chomp.upcase
+  if input == 'Y'
+    @rounds = 0
+    play
+  end
+  exit
 end
 
 class Human
   attr_accessor :name
 
   def initialize
-    player_name
+    @name = ''
   end
 
   def player_name
@@ -140,7 +165,7 @@ class Human
 end
 
 # class Computer
-class Computer < Game
+class Computer
   attr_accessor :name
 
   def initialize
@@ -157,3 +182,4 @@ class Computer < Game
 end
 
 game = Game.new
+game.play
